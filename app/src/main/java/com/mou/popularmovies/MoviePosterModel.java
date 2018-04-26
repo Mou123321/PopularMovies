c3ce37773be19118886f860aaeae2ba5e1f00aec
Mou123321 <mosun@ucsd.edu>
2018-04-26 15:48:08 -0700

    added model class and list item layout]

---
+++ app/src/main/java/com/mou/popularmovies/MoviePosterModel.java
@@ -0,0 +1,57 @@
+package com.mou.popularmovies;
+
+public class MoviePosterModel {
+    String image;
+    String title;
+    String synopsis;
+    String rating;
+    String releaseDate;
+
+    public MoviePosterModel(String image, String title, String synopsis, String rating, String releaseDate) {
+        this.image = image;
+        this.title = title;
+        this.synopsis = synopsis;
+        this.rating = rating;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
